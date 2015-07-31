1dbfc766b3befde780fd62795d13bfc09de61af1
lambda_sakura <lambda.sakura@gmail.com>
2015-07-31 13:38:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# iotkit-sensor-tag-client
+
+Indel Edison上で動作する、IoTAnalticsと TI の SensorTag を連携させるためのクライアント
+BLEでsensortagと通信して、温度、湿度を取得して、iotkit-agentに対して、udpで情報送信する。
+
+
---
+++ index.js
@@ -0,0 +1,84 @@
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
+var async = require('async');
+var SensorTag = require("sensortag");
+
+var udpOptions = {
+  host : '127.0.0.1',
+  port : 41234
+};
+
+function sendObservation(name, value, on){
+  var msg = JSON.stringify({
+    n: name,
+    v: value,
+    on: on
+  });
+
+  var sentMsg = new Buffer(msg);
+  console.log("Sending observation: " + sentMsg);
+  client.send(sentMsg, 0, sentMsg.length, udpOptions.port, udpOptions.host);
+}
+
+SensorTag.discover(function(sensorTag) {
+
+  sensorTag.on('disconnect', function() {
+    console.log('disconnected SensorTag');
+    process.exit(0);
+  });
+
+  console.log('connect SensorTag');
+
+  sensorTag.connect(function() {
+    sensorTag.discoverServicesAndCharacteristics(function() {
+      sensorTag.enableIrTemperature(function() {
+        sensorTag.enableHumidity(function() {
+          setInterval(function() {
+            async.series([
+              function(callback) {
+                console.log('readIrTemperature');
+                sensorTag.readIrTemperature(function(objectTemperature, ambientTemperature) {
+                  console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+                  console.log('\tambient temperature = %d °C', ambientTemperature.toFixed(1));
+
+                  var obj = Object();
+                  obj['objectTemperature'] = objectTemperature;
+                  obj['ambientTemperature'] = ambientTemperature;
+                  callback(null, obj);
+                });
+              },
+              function(callback) {
+                console.log('readHumidity');
+                sensorTag.readHumidity(function(temperature, humidity) {
+                  console.log('\ttemperature = %d °C', temperature.toFixed(1));
+                  console.log('\thumidity = %d %', humidity.toFixed(1));
+
+                  var obj = Object();
+                  obj['temperature'] = temperature;
+                  obj['humidity'] = humidity;
+                  callback(null, obj);
+                });
+              }
+            ],
+                         function(err, results) {
+                           console.log('results');
+                           console.log(results);
+
+                           var date = new Date();
+                           console.log("==== " + date + " ====");
+                           var temp = tempPin.value();
+                           var light = lightPin.value();
+
+                           console.log(tempPin.name() + ": " + temp);
+                           sendObservation('temp', temp, date.getTime());
+
+                           console.log(lightPin.name() + ": " + light);
+                           sendObservation('humidity', light, date.getTime());
+                         }
+                        );
+          }, 1000*10);
+        });
+      });
+    });
+  });
+});
